ced382fa0c4787ebb9b37f8d34615146abcdea8e
sakuraottero-pixel <sakuraottero@gmail.com>
2025-12-25 14:24:30 +0600

    Add CORS middleware to Express app

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import fetch from "node-fetch";
 import crypto from "crypto";
+import cors from "cors";
+
 
 const app = express();
+app.use(cors());    
 app.use(express.json());
 
 // Env vars
